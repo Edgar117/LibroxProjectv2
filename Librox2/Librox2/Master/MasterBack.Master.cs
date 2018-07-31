295290d5a84dc22e18cbaed1e2dc9a8d6c037afb
Edgar117 <huesos_blin182@hotmail.com>
2018-07-31 13:45:14 -0500

    Message

--- Librox2/Librox2/Master/MasterBack.Master.cs
+++ Librox2/Librox2/Master/MasterBack.Master.cs
@@ -11,6 +11,31 @@ namespace Librox2.Master
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Sirve para mantener el menu abierto y que no se cierre cuando es class Treeview
+            //String activepage = Request.RawUrl;
+            //switch (activepage)
+            //{
+            //    case "/DBTestSite/Configure_Test.aspx":
+            //        test.Attributes.Add("class", "treeview active menu-open");
+            //        //ULTest.Attributes.Add("class", "sidebar-menu .active > .treeview-menu");
+            //        EMPTY.Attributes.Add("class", "active");
+            //        break;
+            //    case "/DBTestSite/Configure_Scripts.aspx":
+            //        test.Attributes.Add("class", "treeview active menu-open");
+            //        Script.Attributes.Add("class", "active");
+            //        break;
+            //    case "/DBTestSite/Configure_Scripts_ftp.aspx":
+            //        test.Attributes.Add("class", "treeview active menu-open");
+            //        ScriptFTP.Attributes.Add("class", "active");
+            //        break;
+            //    case "/DBTestSite/Test_Sass_server.aspx":
+            //        test.Attributes.Add("class", "treeview active menu-open");
+            //        SassServer.Attributes.Add("class", "active");
+            //        break;
+            //    default:
+            //        ULTest.Attributes.Add("class", "treeview-menu");
+            //        break;
+            //}
             try
             {
                 nom.InnerText = Session["Usuario"].ToString();
