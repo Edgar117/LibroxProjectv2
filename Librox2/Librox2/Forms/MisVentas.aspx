9e9fe0b704085ab8cd78647466c35a07e71c0158
David May <davisaac8@gmail.com>
2019-02-23 00:22:50 -0600

    Mis ventas

---
+++ Librox2/Librox2/Forms/MisVentas.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/MasterPro.Master" AutoEventWireup="true" CodeBehind="MisVentas.aspx.cs" Inherits="Librox2.Forms.MisVentas" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="wrapper">
+        <div class="page-header page-header-small">
+            <div class="page-header-image" data-parallax="true" style="background-image: url('../Maybe/assets/img/bg6.jpg');">
+            </div>
+            <div class="content-center">
+                <div class="container">
+                    <h1 class="title">Escribox.</h1>
+                </div>
+            </div>
+        </div>
+        <div class="section section-contact-us text-center">
+            <div class="container">
+                <h2 class="title">Mis ventas</h2>
+                <h4>*El porcentaje ganado hace referencia al 65% del precio original. La comisión de Escribox es un 35% del precio total</h4>
+                <div class="row">
+                    <div class="col-md-12">
+                        <asp:GridView ID="dtgVentas" runat="server" CssClass="table table-bordered">
+                        </asp:GridView>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                    <h3 class="text-right">Total de ventas:</h3>
+                    <h2 class="text-right">$ 
+                        <asp:Label ID="lblTotal" runat="server" Text="Label"></asp:Label>
+                        <br />
+                        <asp:Label ID="Label1" runat="server" Text="*Los pagos se realizan cada día 30. Dudas o aclaraciones comunicarse a escribox" forecolor="Orange"></asp:Label>
+                    </h2>
+                        <div class="pull-right">
+                    <asp:Button ID="btnSolicitar" runat="server" Text="Solicitar fondos" OnClick="btnSolicitar_Click" CssClass="btn btn-warning btn-round" />
+                            </div>
+                </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Librox2/Librox2/Forms/MisVentas.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Librox2.DAO;
+
+namespace Librox2.Forms
+{
+    public partial class MisVentas : System.Web.UI.Page
+    {
+        int id = 0;
+        String[] cart1 = new String[0];
+        LibrosDAO books = new LibrosDAO();
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cart1 = (String[])Session["ALL"];
+            id = int.Parse(cart1[5]);
+            //Bind datagrid
+            try
+            {
+                dt = books.ConsultarMiPago(id); //Devuelve solo ventas que no se haya solicitado fondos
+                dtgVentas.DataSource = dt;
+                dtgVentas.DataBind();
+                getTotal();
+            }
+            catch (Exception ex)
+            {
+                lblTotal.Text = ex.Message;
+            }
+
+        }
+
+        protected void btnSolicitar_Click(object sender, EventArgs e)
+        {
+            Decimal subTotal = Convert.ToDecimal(lblTotal.Text);
+            if (subTotal >= 10)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    int idPago = Convert.ToInt32(row["IDPago"]);
+                    string var = row["Libro"].ToString();
+                    string precioAPagar = row["Precio Original"].ToString();
+                    decimal ganado = Convert.ToDecimal(row["% Obtenido"]);
+                    string escribox = row["Comisión Escribox"].ToString();
+                    //Rutina para actualizar el pago en tabla ventas
+                    books.ActualizarPagoUsuario(idPago);
+                }
+                //Inserta el  pago en tabla pagos
+                books.InsertarPagoUsuario(id, 0, subTotal);
+            }
+            Response.Redirect("~/Forms/MisVentas.aspx");
+        }
+        protected void getTotal()
+        {
+            Decimal total = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                decimal monto = Convert.ToDecimal(row["% Obtenido"]);
+                total = Convert.ToDecimal(total + monto);
+            }
+            lblTotal.Text = total.ToString();
+        }
+    }
+}
