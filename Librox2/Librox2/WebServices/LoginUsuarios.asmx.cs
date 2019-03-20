c5b9c60605e15f5ecfc7ac9b2eaf2ba851cb7da0
Edgar117 <huesos_blin182@hotmail.com>
2019-03-19 18:30:49 -0600

    Cambio correcto a URl para la movil

--- Librox2/Librox2/WebServices/LoginUsuarios.asmx.cs
+++ Librox2/Librox2/WebServices/LoginUsuarios.asmx.cs
@@ -12,7 +12,7 @@ namespace Librox2.WebServices
     /// <summary>
     /// Descripción breve de LoginUsuarios
     /// </summary>
-    [WebService(Namespace = "http://boxescri-001-site1.atempurl.com/WebServices/")]
+    [WebService(Namespace = "https://www.escribox.com/WebServices/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
